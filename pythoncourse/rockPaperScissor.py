e6948fa009b18ee7cbfc85c30221416b5c8088b6
thando mini <thando@sasoft.co.za>
2024-02-27 13:28:19 +0200

    created a small console rock paper scissors game. very basic. Revising conditionals in python

---
+++ pythoncourse/rockPaperScissor.py
@@ -0,0 +1,13 @@
+computer_choice = 'scissors'
+user_choice = input('Do you want rock, paper or scissors? ')
+
+if computer_choice == user_choice:
+    print('Tie!')
+elif user_choice == 'rock' and computer_choice == 'scissors':
+    print('You win!')
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print('You win!')
+elif user_choice == 'scissors' and computer_choice == 'paper':
+    print('You win!')
+else:
+    print('You lose!')
