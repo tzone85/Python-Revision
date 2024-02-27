ee79de4d19b938691f3f3e4fb2e45e0284e226b8
thando mini <thando@sasoft.co.za>
2024-02-28 00:50:23 +0200

    basically delt with lists, and how one can traverse a loop, including performing some basic operations with the lists contents

---
+++ pythoncourse/sumExpenses.py
@@ -0,0 +1,12 @@
+
+expenses = [10, 50, 8, 5, 15, 20, 5, 3]
+
+# sum  = 0
+
+# you can actually skip this and use the built in function sum
+# for expense in expenses:
+#     sum += expense
+
+total = sum(expenses)
+# print("You spent $" + str(sum) + " this week.", sep="")
+print("You spent $", total, " this week.", sep="")
