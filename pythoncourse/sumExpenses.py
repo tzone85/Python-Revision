3188a56022b3a5b0dfccfcbd32b1e600615f0bf6
thando mini <thando@sasoft.co.za>
2024-02-28 01:34:33 +0200

    This adds up all the number of expenses that get collected in one go within a for loop

--- pythoncourse/sumExpenses.py
+++ pythoncourse/sumExpenses.py
@@ -1,11 +1,8 @@
 
-expenses = [10, 50, 8, 5, 15, 20, 5, 3]
-
-# sum  = 0
-
-# you can actually skip this and use the built in function sum
-# for expense in expenses:
-#     sum += expense
+expenses = []
+total = 0
+for i in range(7):
+    expenses.append(float(input("Enter an expense: ")))
 
 total = sum(expenses)
 # print("You spent $" + str(sum) + " this week.", sep="")
